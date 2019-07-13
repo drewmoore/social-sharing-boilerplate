04fa7f218ed8cb9283059809de18d430a52242b2
Andrew Moore <drewmoore@users.noreply.github.com>
2019-07-14 00:50:16 +0200

    Points build folder to public, ignores build dir.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
-app.use(express.static('dist'));
+app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'public/js')
   },
   mode: process.env.WEBPACK_BUILD_MODE,
   module: {
