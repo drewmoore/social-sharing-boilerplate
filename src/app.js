37bfaf42a3c28b78fd590606aa04b88ca0cd2e9e
Andrew Moore <drewmoore@users.noreply.github.com>
2019-07-14 03:11:43 +0200

    Render html in express.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,24 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send(`
+    <!doctype html>
+    <html>
+      <head>
+        <title>Social Sharing Boilerplate</title>
+      </head>
+      <body>
+        <div id="root"></div>
+        <script src="js/main.js"></script>
+      </body>
+    </html>
+  `);
+});
+
 app.use(express.static('public'));
+app.use(router);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
